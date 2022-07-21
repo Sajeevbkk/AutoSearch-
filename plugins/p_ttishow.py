742548a9c8385a735549cdf7e57c3afcf9ae8922
Sajeevbkk <96622442+Sajeevbkk@users.noreply.github.com>
2022-07-21 17:13:36 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -103,9 +103,18 @@ async def list_chats(bot, message):
             outfile.write(out)
         await message.reply_document('chats.txt', caption="List Of Chats")
 
-        
+
+@Client.on_message(filters.command('clone') & filters.incoming)
+async def get_clone(bot, message):
+    await message.reply_text("<b>Hi, Guys I'am a Developer, If You Need A Bot Like This => See @AdvBotUpdates We Will Also Develope Bot Like Mallu_Movies/ProSearch bot and We Will Also Develope Bot As Your Wish😇</b>")
+
 @Client.on_message(filters.command('hello') & filters.incoming)
 async def get_clone(bot, message):
-    await message.reply_text("<b>Hi Guys, If You Need Movies Then Ask It Here => See @FilmPiratesGroup OR Click Ask Movies😇</b>")
-
-   
+    await message.reply_text("<b>Hi Guys, If You Need Movies Then Ask It Here => See @FilmPiratesGroup OR Click Ask Movies😇</b>"),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Ask Movies 😇", url=f"http://t.me/FilmPiratesOfficial"
+                    )
+                ]
